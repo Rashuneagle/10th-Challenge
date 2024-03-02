7c81b1552be152ea1a59659088fec35ece618809
Michael Scott <mscotteagle21@gmail.com>
2024-03-02 15:58:30 -0600

    added shape detail to shape classes added a parent shape class, added required packages into files.

--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -1,14 +1,45 @@
+class Shape {
+    constructor() {
 
+    }
+}
 
-class Triangle {
+class Triangle extends Shape {
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        return '<polygon points="150, 18 244, 182 56, 182" fill="blue" />';
+
+    }
 
 }
 
-class Circle {
+class Circle extends Shape {
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        return '<circle cx="150" cy="100" r="80" fill="red" />';
+    }
 
 }
 
-class Square {
+class Square extends Shape {
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        return '<rect x="50" y="50" width="100" height="100" fill="green" />';
+    }
 
 
 }
+
+module.exports = { Triangle, Circle, Square };
