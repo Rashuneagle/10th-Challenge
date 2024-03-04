63423dc9cdff8b29fbe2db00c5d65c5d36a8f97a
Michael Scott <mscotteagle21@gmail.com>
2024-03-03 19:20:58 -0600

    started on testing file for shapes, added question prompts for features of the logo

--- Main/index.js
+++ Main/index.js
@@ -1,1 +1,33 @@
 const inquirer = require('inquirer');
+
+
+
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter 3 letters for your logo',
+        name: 'title',
+
+    },
+    
+    {
+        type: 'input',
+        name: 'color',
+        message: 'What do you want the text color to be?',
+    },
+
+    {
+        type: 'input',
+        message: 'What shape do you want?',
+        name: 'shape',
+        choices: ['Circle', 'Triangle', 'Square'],
+    },
+    
+    {
+        type: 'input',
+        message: 'What is the shapes color?',
+        name: 'shape color',
+    },
+
+    
+]
--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -11,7 +11,7 @@ class Triangle extends Shape {
     }
 
     render() {
-        return '<polygon points="150, 18 244, 182 56, 182" fill="blue" />';
+        return '<polygon points="150, 18 244, 182 56, 182"/>';
 
     }
 
@@ -24,7 +24,7 @@ class Circle extends Shape {
     }
 
     render() {
-        return '<circle cx="150" cy="100" r="80" fill="red" />';
+        return '<circle cx="150" cy="100" r="80" />';
     }
 
 }
@@ -36,7 +36,7 @@ class Square extends Shape {
     }
 
     render() {
-        return '<rect x="50" y="50" width="100" height="100" fill="green" />';
+        return '<rect x="50" y="50" width="100" height="100" />';
     }
 
 
