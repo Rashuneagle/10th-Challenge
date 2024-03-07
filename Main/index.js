a403b5cd606f12af5aaadc90899b196e693f6971
Michael Scott <mscotteagle21@gmail.com>
2024-03-06 20:19:39 -0600

    created function to initialize app and to start the prompting process, also started on file to write svg file.

--- Main/index.js
+++ Main/index.js
@@ -1,39 +1,44 @@
 const inquirer = require('inquirer');
-const { Triangle, Circle, Square } = require('./shapes');
-
-
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
 const questions = [
     {
         type: 'input',
         message: 'Enter 3 letters for your logo',
         name: 'title',
-
     },
-    
     {
         type: 'input',
         name: 'color',
         message: 'What do you want the text color to be?',
     },
-
     {
-        type: 'input',
+        type: 'list',
         message: 'What shape do you want?',
         name: 'shape',
         choices: ['Circle', 'Triangle', 'Square'],
     },
-    
     {
         type: 'input',
-        message: 'What is the shapes color?',
-        name: 'shape color',
+        message: 'What is the shape color?',
+        name: 'shapeColor',
     },
-
-    
 ];
 
 // Function to create SVG file
 function writeToFile(fileName, data) {
     
 }
+
+// Function to handle user input
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        // Generate content using shapes function
+        const shapeContent = shapes(answers.title, answers.color, answers.shape, answers.shapeColor);
+        // Use writeToFile function to save the content to a file
+        writeToFile('logo.svg', shapeContent);
+    });
+}
+
+
+init();
