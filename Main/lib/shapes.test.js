dc318ef39f279f6935cb4f3277a690ac73600bd9
Michael Scott <mscotteagle21@gmail.com>
2024-03-13 18:35:15 -0500

    tried to fix test suite, and removed required package from index.js file

--- Main/lib/shapes.test.js
+++ Main/lib/shapes.test.js
@@ -1,39 +1,27 @@
 const { Triangle, Circle, Square } = require('./shapes');
 
-
-
 describe('shapes', () => {
-
     describe('Triangle', () => {
         it('Should render a triangle shape', () => {
             const triangle = new Triangle();
             expect(triangle.render())
-            .toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue"/>');
+                .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"> <polygon points="150, 18 244, 182 56, 182" fill="blue"/>         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${triangle.color}">${triangle.title}</text> </svg>`);
         });
     });
-});
 
-describe('shapes', () => {
-    
     describe('Circle', () => {
         it('Should render a circle shape', () => {
             const circle = new Circle();
             expect(circle.render())
-            .toEqual('<circle cx="150" cy="100" r="80" />');
-            
-
+                .toEqual('<circle cx="150" cy="100" r="80" />');
         });
     });
-});
 
-describe('shapes', () => {
-    
     describe('Square', () => {
         it('Should render a square shape', () => {
             const square = new Square();
             expect(square.render())
-            .toEqual('<rect x="50" y="50" width="100" height="100" />');
-
+                .toEqual('<rect x="50" y="50" width="100" height="100" />');
         });
     });
 });
